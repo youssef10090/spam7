b66d3c9f470ae3fa12f6d7d5fff8ff0509f35813
youssef10090 <30448957+youssef10090@users.noreply.github.com>
2019-06-05 06:59:34 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("585533446487801877")
+setInterval(function() {
+channel.send(`youssef10090 زعيم كبير youssef10090 زعيم كبير youssef10090 زعيم كبير youssef10090 زعيم كبير `);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
